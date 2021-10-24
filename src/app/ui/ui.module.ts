e0628a983b4c249395fd79f4392dd74bf3728814
Wojciech Dudek <wdudek82@gmail.com>
2021-10-24 11:39:43 +0200

    user-story-5: cleanup styles

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -2,16 +2,13 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {TopbarComponent} from './components/topbar/topbar.component';
 import {BbUIModule} from "../bb-ui/bb-ui.module";
-import {MainFooterComponent} from './components/main-footer/main-footer.component';
 
 @NgModule({
   declarations: [
     TopbarComponent,
-    MainFooterComponent
   ],
   exports: [
     TopbarComponent,
-    MainFooterComponent,
   ],
   imports: [
     CommonModule,
